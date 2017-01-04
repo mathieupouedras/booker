0e9261f0dd06e0343776dc88faa587a1e37f3294
mpouedras <Mathieu.POUEDRAS@cgos.asso.fr>
2017-01-04 17:26:14 +0100

    splitting login from booking

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const fs = require('fs')
 const https = require('https')
+const FormData = require('form-data')
+
+function login(parameters) {
+  const form = new FormData()
+  form.append('username', parameters.username1)
+  form.append('password', parameters.password1)
+  form.append(parameters.organizationIdentifierName, parameters.organizationIdentifierValue)
+
+  const options = {
+  	hostname: parameters.hostname,
+  	path: parameters.loginPath,
+  	method: 'POST',
+  	headers: form.getHeaders()
+  }
+
+  const request = https.request(options, (res) => {
+  	console.log(`statusCode: ${res.statusCode}`)
+  	// Lister les cookies et prendre celui que je veux
+  	console.log(res.headers['set-cookie'][1].split(';')[0].split('=')[1])
+
+  	res.on('data', (d) => {
+			process.stdout.write(d)
+  	})
+  })
+
+  form.pipe(request)
+
+  request.on('error', (e) => {
+  	console.error(e)
+  })
+
+  request.end()
+}
 
 try {
   const data = fs.readFileSync('application.json', 'UTF-8')
   const jsonData  = JSON.parse(data)
-  
-  https.get(jsonData.home, (res) => {
-    const firstParam = res.headers['set-cookie'][0].split(';')[0].split('=')[1]
-    const secondParam = res.headers['set-cookie'][1].split(';')[0].split('=')[1]
-    const thirdParam = res.headers['set-cookie'][2].split(';')[0].split('=')[1]
 
-    console.log(thirdParam)
-  })
+  login(jsonData)
 
 } catch(e) {
   console.log(`Error : ${e.stack}`)
---
+++ promise.js
@@ -0,0 +1,1 @@
+//retourer une promise apres post 
